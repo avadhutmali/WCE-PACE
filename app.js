4d4a32ed3f3b617ef514fb649521e31ffae9b361
Avadhut <mali.avadhut7@gmail.com>
2024-09-13 11:45:44 +0530

    Website

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
 let welcome = document.querySelector("h1");
-let optionsSilder = document.querySelector(".optionsSilder")
-let menu = document.querySelector(".options")
-let closeB = document.querySelector(".close")
+let optionsSilder = document.querySelector(".optionsSilder");
+let menu = document.querySelector(".options");
+let closeB = document.querySelector(".close");
 
 $(welcome).textillate({
-    in :{effect:"fadeIn"},
-})
-welcome.addEventListener("mouseenter",()=>{
-     $(welcome).textillate('in');
-})
+    in: { effect: "fadeIn" },
+});
 
-let tl = gsap.timeline()
+welcome.addEventListener("mouseenter", () => {
+    $(welcome).textillate('in');
+});
 
-tl.to(".optionsSilder",{
-    right :"0",
-    duration :0.5,
-})
-tl.pause();
+let tl = gsap.timeline({ paused: true });
 
-menu.addEventListener("click",()=>{tl.play()})
-closeB.addEventListener("click",()=>{tl.reverse()})
+tl.to(".optionsSilder", {
+    right: "0",
+    duration: 0.5,
+    ease: "power2.inOut"
+});
 
-    
-    
+menu.addEventListener("click", () => {
+    tl.play();
+});
+
+closeB.addEventListener("click", () => {
+    tl.reverse();
+});
